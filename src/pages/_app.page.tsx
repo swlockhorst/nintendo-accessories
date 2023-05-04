08e1d06cb920ea48c4404c1368cddca7cb3aa997
Steven Lockhorst <swlockhorst@gmail.com>
2023-05-03 19:00:56 -0700

    Update _app.page.tsx

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,15 +1,17 @@
 import { Urbanist } from '@next/font/google';
+import { Layout } from '@src/components/templates/layout';
+// eslint-disable-next-line import/no-unresolved
+import { Analytics } from '@vercel/analytics/react';
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import './utils/globals.css';
-
-import { Layout } from '@src/components/templates/layout';
 
 const urbanist = Urbanist({ subsets: ['latin'], variable: '--font-urbanist' });
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <Analytics />
       <main className={`${urbanist.variable} font-sans`}>
         <Layout>
           <Component {...pageProps} />
